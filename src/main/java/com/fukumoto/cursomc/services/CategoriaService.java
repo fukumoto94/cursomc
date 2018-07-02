e27b7488170f95ee2736643d002737f1be546a96
Lucas Fukumoto <lucas.fukumoto@hotmail.com>
2018-07-02 20:37:13 -0300

    Deletando e 2 aulas anteriores

--- src/main/java/com/fukumoto/cursomc/services/CategoriaService.java
+++ src/main/java/com/fukumoto/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.fukumoto.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.fukumoto.cursomc.domain.Categoria;
 import com.fukumoto.cursomc.repositories.CategoriaRepository;
+import com.fukumoto.cursomc.services.exceptions.DataIntegrityException;
 import com.fukumoto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,4 +22,23 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado Id:"+id+", Tipo: "+Categoria.class.getName())) ;
 	}
 	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
 }
